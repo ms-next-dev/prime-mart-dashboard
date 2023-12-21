1a2cd41344af0f3311c7e453a3f411a914b1bfdf
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-12-21 09:07:35 +0600

    foxed: lint error

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -11,40 +11,38 @@ import { useEffect } from "react";
 import { BillboardColumn, columns } from "./column";
 
 interface BillboardClientProps {
-    data: BillboardColumn[];
+  data: BillboardColumn[];
 }
 
 const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
-    const router = useRouter();
-    const params = useParams();
+  const router = useRouter();
+  const params = useParams();
 
-    useEffect(() => {
-        router.refresh();
-    }, []);
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
 
-    return (
-        <div>
-            <div className="flex items-center justify-between">
-                <Heading
-                    title={`Billboards (${data.length})`}
-                    description="Manage billboards for your store"
-                />
-                <Button
-                    onClick={() =>
-                        router.push(`/${params.storeId}/billboards/new`)
-                    }
-                >
-                    <Plus className="mr-2 h-4 w-4" />
-                    Add New
-                </Button>
-            </div>
-            <Separator />
-            <DataTable columns={columns} data={data} searchKey="label" />
-            <Heading title="Api" description="Api calls for Billboards" />
-            <Separator />
-            <ApiList entityName="billboards" entityIdName="billboardId" />
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="Api" description="Api calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
+    </div>
+  );
 };
 
 export default BillboardClient;
