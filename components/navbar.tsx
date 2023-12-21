7afdee3b7647f8414fe8584bfc3cc7d0cbac819f
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-12-21 08:47:22 +0600

    added: theme togglle feature

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,31 +6,33 @@ import prismaDb from "@/lib/prismaDb";
 import { redirect } from "next/navigation";
 import MainNav from "./main-nav";
 import { StoreSwitcher } from "./store-switcher";
+import { ModeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
-    const { userId } = auth();
+  const { userId } = auth();
 
-    if (!userId) {
-        redirect("/sign-in");
-    }
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
-    const stores = await prismaDb.store.findMany({
-        where: {
-            userId,
-        },
-    });
+  const stores = await prismaDb.store.findMany({
+    where: {
+      userId,
+    },
+  });
 
-    return (
-        <div className="border-b">
-            <div className="flex h-16 items-center px-4">
-                <StoreSwitcher items={stores} />
-                <MainNav className="mx-6" />
-                <div className="ml-auto flex items-center space-x-4">
-                    <UserButton afterSignOutUrl="/" />
-                </div>
-            </div>
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
